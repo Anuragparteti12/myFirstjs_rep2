de99bbb52d77a8259ebf626cfb6fd06fecca1061
Anuragparteti12 <148538716+Anuragparteti12@users.noreply.github.com>
2023-10-30 05:24:38 +0000

    Changes Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=12564;
+let accountEmail="anurag@gmail.com";
+var accountPassword="123545";
+accountCity="jaipur";
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
